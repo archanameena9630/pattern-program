3e5a4d7dfade54cbf17ccde94d6cbe1fa76f1804
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-09-06 23:55:28 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class pattern2 {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println(" enter a number");
+        int num=obj.nextInt();
+                for (int i=1;i<=num;i++){
+                    for(int j=1;j<=i;j++){
+             System.out.print("*");
+            }
+         System.out.println();
+        }
+                }
+            }
+        
+    
+
