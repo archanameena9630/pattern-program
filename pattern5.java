e7d89941708427c71cb085d85b9c48701ac50836
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-09-18 23:35:31 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,24 @@
+// 1
+// 22
+// 333
+// 4444
+// 55555
+    import java.util.Scanner;
+  public class pattern5 {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println(" enter a number");
+        int num=obj.nextInt();
+                for (int i=1;i<=num;i++){
+             for ( int j=1;j<=i;j++){
+             System.out.print(i);
+            }
+         System.out.println();
+        }
+    }
+}
+
+
+
+    
+
